a67ca8239933f38f0e9a0e042d274baaf02b4d16
PhilipMouridsen <pcm@nine.dk>
2024-10-22 10:54:41 +0200

    eslint update

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,22 @@
-import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
-import pluginVitest from '@vitest/eslint-plugin'
-import pluginCypress from 'eslint-plugin-cypress/flat'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginVue from 'eslint-plugin-vue';
+import vueTsEslintConfig from '@vue/eslint-config-typescript';
+import pluginVitest from '@vitest/eslint-plugin';
+import pluginCypress from 'eslint-plugin-cypress/flat';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
 
 export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
@@ -15,20 +27,19 @@ export default [
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
 
-  ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
+
   {
     ...pluginCypress.configs.recommended,
     files: [
       'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-      'cypress/support/**/*.{js,ts,jsx,tsx}'
+      'cypress/support/**/*.{js,ts,jsx,tsx}',
     ],
   },
   skipFormatting,
-]
+];
--- src/components/__tests__/TheMap.spec.ts
+++ src/components/__tests__/TheMap.spec.ts
@@ -1,9 +1,8 @@
 import { describe, it, expect } from "vitest";
-
 import { mount } from "@vue/test-utils";
 import TheMap from "../TheMap.vue";
 
-describe("HelloWorld", () => {
+describe("TheMap", () => {
   it("renders properly", () => {
     const wrapper = mount(TheMap);
     expect(wrapper).toBeCalled();
